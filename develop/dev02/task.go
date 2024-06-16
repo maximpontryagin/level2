55804522066b1457ab5e9f415efe669b27fbf40d
Максим Понтрягин <maximpontryagin@yandex.ru>
2024-06-16 16:29:43 +0300

    add dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -18,6 +27,51 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+const escapeSymbolUnicode = 92 // номер обратного слеша в Unicode
 
+func unpackString(str string) (string, error) {
+	if len(str) == 0 {
+		return "", nil
+	}
+	if _, err := strconv.Atoi(string(str[0])); err == nil {
+		return "", errors.New("incorrect string")
+	}
+
+	runes := []rune(str)
+	var result strings.Builder
+
+	for i := 0; i < len(runes); i++ {
+		if runes[i] == escapeSymbolUnicode {
+			i++ // Переходим на символ который нужно продублировать
+		}
+		toWrite := string(runes[i])
+
+		if i+1 != len(runes) {
+			if unicode.IsDigit(runes[i+1]) {
+				number, err := strconv.Atoi(string(runes[i+1]))
+				if err != nil {
+					return "", errors.New("ошибка при конвертации строки в число")
+				}
+				for j := 0; j < number; j++ {
+					result.WriteString(toWrite) // Запись символа указанного колличества раз
+				}
+				i++ // перешагиваем цифру
+			} else {
+				result.WriteString(toWrite) // Запись символа после которого нету цифры
+			}
+		} else {
+			result.WriteString(toWrite) // Запись последнего символа
+		}
+	}
+	return result.String(), nil
+}
+
+func main() {
+	var str string
+	fmt.Scan(&str)
+	unpacked, err := unpackString(str)
+	if err != nil {
+		log.Println("Error:", err)
+	}
+	fmt.Println(unpacked)
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,33 @@
 package main
+
+import "testing"
+
+func TestUnpackString(t *testing.T) {
+	list_tests := []struct {
+		input    string // Введенная строка
+		expected string // Ожидаемный результат
+		errTest  bool   // true если ошибка ожидается, false если не ожидается
+	}{
+		{"a4bc2d5e", "aaaabccddddde", false},
+		{"abcd", "abcd", false},
+		{"45", "", true},
+		{"", "", false},
+		{"qwe\\4\\5", "qwe45", false},
+		{"qwe\\45", "qwe44444", false},
+		{"qwe\\\\5", "qwe\\\\\\\\\\", false},
+	}
+
+	for _, test := range list_tests {
+		test_result, err := unpackString(test.input)
+		if err != nil && test.errTest == false {
+			t.Error("Получили ошибку там где недолжны были. Ошибка: ", err, "Ожидал: ", test.expected, "Поличил: ", test_result)
+		}
+		if err == nil && test.errTest == true {
+			t.Error("Не получили ошибку там где должны были ее получить.", "Ожидал: ", test.expected, "Поличил: ", test_result)
+		}
+
+		if test_result != test.expected {
+			t.Error("Ожидаемый и полученный результат не совпали. Ожидал: ", test.expected, "Поличил: ", test_result)
+		}
+	}
+}
