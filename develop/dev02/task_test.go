de4e70b270673f1f638381615faa8f481db31ece
Максим Понтрягин <maximpontryagin@yandex.ru>
2024-06-16 16:31:05 +0300

    add dev02

--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestUnpackString(t *testing.T) {
-	list_tests := []struct {
+	listTests := []struct {
 		input    string // Введенная строка
 		expected string // Ожидаемный результат
 		errTest  bool   // true если ошибка ожидается, false если не ожидается
@@ -17,17 +17,17 @@ func TestUnpackString(t *testing.T) {
 		{"qwe\\\\5", "qwe\\\\\\\\\\", false},
 	}
 
-	for _, test := range list_tests {
-		test_result, err := unpackString(test.input)
+	for _, test := range listTests {
+		testResult, err := unpackString(test.input)
 		if err != nil && test.errTest == false {
-			t.Error("Получили ошибку там где недолжны были. Ошибка: ", err, "Ожидал: ", test.expected, "Поличил: ", test_result)
+			t.Error("Получили ошибку там где недолжны были. Ошибка: ", err, "Ожидал: ", test.expected, "Поличил: ", testResult)
 		}
 		if err == nil && test.errTest == true {
-			t.Error("Не получили ошибку там где должны были ее получить.", "Ожидал: ", test.expected, "Поличил: ", test_result)
+			t.Error("Не получили ошибку там где должны были ее получить.", "Ожидал: ", test.expected, "Поличил: ", testResult)
 		}
 
-		if test_result != test.expected {
-			t.Error("Ожидаемый и полученный результат не совпали. Ожидал: ", test.expected, "Поличил: ", test_result)
+		if testResult != test.expected {
+			t.Error("Ожидаемый и полученный результат не совпали. Ожидал: ", test.expected, "Поличил: ", testResult)
 		}
 	}
 }
