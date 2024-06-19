861d2a25aaf75539da77f30872a73b0f98a1590a
Максим Понтрягин <maximpontryagin@yandex.ru>
2024-06-19 18:12:38 +0300

    add dev06

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +23,78 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type cutConfig struct {
+	fields    int
+	delimiter string
+	separated bool
+}
 
+func (cfg *cutConfig) parseConfig() {
+	flag.IntVar(&cfg.fields, "f", 0, "выбрать поля (колонки)")
+	flag.StringVar(&cfg.delimiter, "d", "\t", "использовать другой разделитель")
+	flag.BoolVar(&cfg.separated, "s", false, "только строки с разделителем")
+
+	flag.Parse()
+}
+
+func readLinesFromFile(fileName, delimiter string) ([][]string, error) {
+	var result [][]string
+	file, err := os.Open(fileName)
+	if err != nil {
+		return result, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		resLine := strings.Split(scanner.Text(), delimiter)
+		result = append(result, resLine)
+	}
+	return result, scanner.Err()
+}
+
+func getResultByField(columns [][]string, numberField int, separated bool, delimiter string) ([][]string, error) {
+	// getResultByField возвращает [][]string по указанному номеру колонки
+	var result [][]string
+	for _, line := range columns {
+		// Если не нужно выводить колонки без разделителя, то проверяем входит ли delimiter(разделитель) в строку
+		if separated && !strings.Contains(strings.Join(line, delimiter), delimiter) {
+			continue
+		}
+		// Проверка что номер колонки из флага не выходит за пределы слайса
+		if numberField < len(line) {
+			result = append(result, []string{line[numberField]})
+		} else {
+			return nil, errors.New("номер колонки вышел за пределы")
+		}
+	}
+	return result, nil
+}
+
+func main() {
+	var cfg cutConfig
+	cfg.parseConfig()
+	args := flag.Args()
+	if len(args) < 1 {
+		log.Println("Пропущено название файла")
+		os.Exit(1)
+	}
+	// Название файла из которого необходимо взять данные
+	inputFile := args[len(args)-1]
+
+	resultReadLine, err := readLinesFromFile(inputFile, cfg.delimiter)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	// Получаем конечный результат
+	result, err := getResultByField(resultReadLine, cfg.fields, cfg.separated, cfg.delimiter)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	// Вывод результата
+	for _, line := range result {
+		fmt.Println(line)
+	}
 }
--- develop/dev06/task_test.go
+++ develop/dev06/task_test.go
@@ -1,1 +1,128 @@
 package main
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+// Вспомогательная функция для создания временного файла
+func createTestFile(content string) (string, error) {
+	file, err := os.CreateTemp("", "testfile")
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(content)
+	if err != nil {
+		return "", err
+	}
+
+	return file.Name(), nil
+}
+
+func TestReadLinesFromFile(t *testing.T) {
+	tests := []struct {
+		name             string
+		content          string
+		delimiter        string
+		expectedReadLine [][]string
+		separated        bool
+		numberField      int
+		expectedResult   [][]string
+	}{
+		{
+			name:      "базовый случай",
+			content:   "a\tb\tc\nd\te\tf\n",
+			delimiter: "\t",
+			expectedReadLine: [][]string{
+				{"a", "b", "c"},
+				{"d", "e", "f"},
+			},
+			separated:   false,
+			numberField: 0,
+			expectedResult: [][]string{
+				{"a"},
+				{"d"},
+			},
+		},
+		{
+			name:      "другой разделитель",
+			content:   "a,b,c\nd,e,f\n",
+			delimiter: ",",
+			expectedReadLine: [][]string{
+				{"a", "b", "c"},
+				{"d", "e", "f"},
+			},
+			separated:   false,
+			numberField: 0,
+			expectedResult: [][]string{
+				{"a"},
+				{"d"},
+			},
+		},
+		{
+			name:      "Проверка с флагом separated",
+			content:   "a,b,c\nd,e,f\ndef",
+			delimiter: ",",
+			expectedReadLine: [][]string{
+				{"a", "b", "c"},
+				{"d", "e", "f"},
+				{"def"},
+			},
+			separated:   true,
+			numberField: 0,
+			expectedResult: [][]string{
+				{"a"},
+				{"d"},
+			},
+		},
+		{
+			name:      "Проверка без флага separated",
+			content:   "a,b,c\nd,e,f\ndef",
+			delimiter: ",",
+			expectedReadLine: [][]string{
+				{"a", "b", "c"},
+				{"d", "e", "f"},
+				{"def"},
+			},
+			separated:   false,
+			numberField: 0,
+			expectedResult: [][]string{
+				{"a"},
+				{"d"},
+				{"def"},
+			},
+		},
+		{
+			name:      "Проверка второго столбца",
+			content:   "a,b,c\nd,e,f\n",
+			delimiter: ",",
+			expectedReadLine: [][]string{
+				{"a", "b", "c"},
+				{"d", "e", "f"},
+			},
+			separated:   true,
+			numberField: 1,
+			expectedResult: [][]string{
+				{"b"},
+				{"e"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			fileName, _ := createTestFile(test.content)
+			resultReadLine, _ := readLinesFromFile(fileName, test.delimiter)
+			if !reflect.DeepEqual(test.expectedReadLine, resultReadLine) {
+				t.Errorf("1. Ожидалось: %v, получено: %v", test.expectedReadLine, resultReadLine)
+			}
+			result, _ := getResultByField(resultReadLine, test.numberField, test.separated, test.delimiter)
+			if !reflect.DeepEqual(test.expectedResult, result) {
+				t.Errorf("2. Ожидалось: %v, получено: %v", test.expectedResult, result)
+			}
+		})
+	}
+}
