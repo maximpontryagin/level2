b0df84fa3de3b9e2d76fc279135b69f24041bb2d
Максим Понтрягин <maximpontryagin@yandex.ru>
2024-06-21 18:00:45 +0300

    add dev10

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 
@@ -15,6 +28,84 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
 
-func main() {
+const protocol = "tcp"
 
+type telnetConfig struct {
+	timeout     string
+	host        string
+	port        string
+	timeoutTime time.Duration
+}
+
+func (cfg *telnetConfig) parse() error {
+	flag.StringVar(&cfg.timeout, "timeout", "10s", "connection timeout")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		return fmt.Errorf("необходимо указать хост и порт")
+	}
+	cfg.host = args[0]
+	cfg.port = args[1]
+
+	timeoutTime, err := time.ParseDuration(cfg.timeout)
+	if err != nil {
+		return err
+	}
+	cfg.timeoutTime = timeoutTime
+	return nil
+}
+
+func telnet(ctx context.Context, timeout time.Duration, address string) error {
+	conn, err := net.DialTimeout(protocol, address, timeout)
+	if err != nil {
+		return fmt.Errorf("ошибка подключения по tcp: %v", err)
+	}
+	// Закрываем соединение
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Printf("Ошибка при закрытии соединения: %v", err)
+		}
+	}()
+
+	done := make(chan struct{})
+
+	go func() {
+		copyTo(os.Stdout, conn)
+		done <- struct{}{}
+	}()
+	go func() {
+		copyTo(conn, os.Stdin)
+		done <- struct{}{}
+	}()
+
+	select {
+	case <-ctx.Done():
+		log.Println("Получен сигнал прерывания, соединение закрывается...")
+	case <-done:
+		log.Println("Соединение закрыто сервером или ввод завершен.")
+	}
+	return nil
+}
+
+func copyTo(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Printf("ошибка в копировании данных: %v", err)
+	}
+}
+
+func main() {
+	var cfg telnetConfig
+	if err := cfg.parse(); err != nil {
+		log.Fatalf("Ошибка при парсинге аргументов: %v", err)
+	}
+
+	address := fmt.Sprintf("%s:%s", cfg.host, cfg.port)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	defer stop()
+
+	if err := telnet(ctx, cfg.timeoutTime, address); err != nil {
+		log.Fatalf("Ошибка в работе telnet клиента: %v", err)
+	}
+
+	<-ctx.Done()
 }
